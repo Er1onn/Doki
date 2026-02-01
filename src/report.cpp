2a123ede91e74cd67cfc1d155ef81f6c4ac0ad3d
delvinakuka <delvinakuka89@gmail.com>
2026-02-01 08:31:05 -0800

    Shtimi i raporteve mjekësore (Delvina)

---
+++ src/report.cpp
@@ -0,0 +1,12 @@
+#include "Report.h"
+#include <iostream>
+
+using namespace std;
+
+void Report::shfaqRaport() {
+    cout << "\n--- RAPORT SHËNDETËSOR ---\n";
+    cout << "Pacientë aktivë: 12\n";
+    cout << "Termine sot: 5\n";
+    cout << "Barnat më të përdorura: Paracetamol, Ibuprofen\n";
+    cout << "Gjendja e përgjithshme: E qëndrueshme\n";
+}
