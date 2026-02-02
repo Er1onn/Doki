55dae9ae535f900710e6433f305090ca5222c723
Er1onn <erionarifi2018@gmail.com>
2026-02-02 09:29:41 +0100

    Version final i projektit Doki: Rregullim logjike ne main.cpp (Erioni)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 
-#include "Patient.h"
-#include "Appointment.h"
+#include "patient.h"
+#include "appointment.h"
 #include "pharmacy.h"
 #include "ambulance.h"
 #include "report.h"
@@ -18,6 +18,7 @@ int main() {
     Report raport;
 
     int zgjedhja;
+    char mbyllja;
 
     do {
         cout << "\n==============================\n";
@@ -27,7 +28,7 @@ int main() {
         cout << "2. Shfaq pacientet\n";
         cout << "3. Shto termin\n";
         cout << "4. Shfaq terminet\n";
-        cout << "5. Menaxho farmacin\n";
+        cout << "5. Farmaci\n";
         cout << "6. Gjej ambulancen me te afert\n";
         cout << "7. Shfaq raport shendetesor\n";
         cout << "0. Dil\n";
@@ -44,6 +45,7 @@ int main() {
             cout << "Mosha: ";
             cin >> mosha;
             pacientet.push_back(Patient(emri, mosha));
+            cout << "Pacienti u shtua me sukses.\n";
             break;
         }
 
@@ -59,9 +61,10 @@ int main() {
             string emri, data;
             cout << "Emri i pacientit: ";
             cin >> emri;
-            cout << "Data: ";
+            cout << "Data e terminit: ";
             cin >> data;
             terminet.push_back(Appointment(emri, data));
+            cout << "Termini u shtua.\n";
             break;
         }
 
@@ -76,13 +79,10 @@ int main() {
         case 5: {
             string emri;
             double cmimi;
-
             cout << "Emri i barit: ";
             cin >> emri;
-
-            cout << "Cmimi i barit: ";
+            cout << "Cmimi: ";
             cin >> cmimi;
-
             farmaci.shtoBarne(emri, cmimi);
             break;
         }
@@ -96,14 +96,18 @@ int main() {
             break;
 
         case 0:
-            cout << "Programi u mbyll me sukses.\n";
-            break;
+            cout << "Programi u mbyll.\n";
+            return 0;
 
         default:
             cout << "Zgjedhje e pavlefshme!\n";
         }
 
-    } while (zgjedhja != 0);
+        cout << "\nDeshironi ta mbyllni programin? (y/n): ";
+        cin >> mbyllja;
 
+    } while (mbyllja == 'n' || mbyllja == 'N');
+
+    cout << "Programi perfundoi.\n";
     return 0;
 }
